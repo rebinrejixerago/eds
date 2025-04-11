1ef0307eedc53fa6327a69c0e677a72471b62f0e
rebinrejixerago <rebinrejixerago@gmail.com>
2025-04-11 09:52:16 +0530

    Update related-articles.js

--- blocks/related-articles/related-articles.js
+++ blocks/related-articles/related-articles.js
@@ -1,1 +1,10 @@
-
+export default function decorate(block) {
+  const articles = block.querySelectorAll('li');
+  const listItems = Array.from(articles).map(li => `<li>${li.textContent}</li>`).join('');
+  block.innerHTML = `
+    <div class="related-articles">
+      <h4>Related Articles</h4>
+      <ul>${listItems}</ul>
+    </div>
+  `;
+}
