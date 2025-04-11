e7ae82a22dffaccc6aab7889ff6a0aeaeae5e319
rebinrejixerago <rebinrejixerago@gmail.com>
2025-04-11 12:13:30 +0530

    Update login-form.js

--- blocks/login-form/login-form.js
+++ blocks/login-form/login-form.js
@@ -1,1 +1,10 @@
-
+export default function decorate(block) {
+  block.innerHTML = `
+    <form class="login-form">
+      <h3>Login</h3>
+      <label>Email:<br><input type="email" required></label><br>
+      <label>Password:<br><input type="password" required></label><br>
+      <button type="submit">Login</button>
+    </form>
+  `;
+}
