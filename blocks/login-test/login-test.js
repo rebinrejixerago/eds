4b130611397060fee5fa83461c43d608d22676a3
rebinrejixerago <rebinrejixerago@gmail.com>
2025-04-15 10:43:25 +0530

    Update login-test.js

--- blocks/login-test/login-test.js
+++ blocks/login-test/login-test.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const iframe = document.createElement('iframe');
-  iframe.src = 'http://localhost:4502/content/dam/formsanddocuments/form-without-model/jcr:content?wcmmode=disabled'; 
+  iframe.src = 'https://medium.com/@toimrank/edge-delivery-services-overview-5c32b974321a'; 
   iframe.width = '100%';
   iframe.height = '600';
   iframe.style.border = 'none';
